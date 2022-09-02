895904f1be67302ba2f13bf84c62c3290f7dff63
Paola Gallegos <54852214+PaolaGAPI@users.noreply.github.com>
2022-09-01 19:48:51 -0500

    Add files via upload

---
+++ criptosite/utils/substitution_system.py
@@ -0,0 +1,57 @@
+# Python program to demonstrate
+# Substitution Cipher
+import string
+
+"""
+create a dictionary to store the substitution
+for the given alphabet in the cipher
+text based on the key
+"""
+
+def cipher_text(plain_txt,key):
+    dict1 = {}
+    cipher_txt = []
+    all_letters = string.ascii_letters
+    for i in range(len(all_letters)):
+        dict1[all_letters[i]] = all_letters[(i + key) % len(all_letters)]
+    for char in plain_txt:
+        if char in all_letters:
+            temp = dict1[char]
+            cipher_txt.append(temp)
+        else:
+            temp = char
+            cipher_txt.append(temp)
+
+    return "".join(cipher_txt)
+
+
+"""
+create a dictionary to store the substitution
+for the given alphabet in the plain text
+based on the key
+"""
+
+def DecryptedText(ciphertext, key):
+    dict2 = {}
+    all_letters = string.ascii_letters
+    for i in range(len(all_letters)):
+        dict2[all_letters[i]] = all_letters[(i - key) % (len(all_letters))]
+    # loop to recover plain text
+    decrypt_txt = []
+    for char in ciphertext:
+        if char in all_letters:
+            temp = dict2[char]
+            decrypt_txt.append(temp)
+        else:
+            temp = char
+            decrypt_txt.append(temp)
+
+    return "".join(decrypt_txt)
+
+
+if __name__ == "__main__":
+    plain_txt = str(input("message:"))  # "I am studying Data Encryption"
+    key = int(input("Key:"))  # 4
+    ciphertext = cipher_text(plain_txt,key)
+    print("Ciphertext :", ciphertext)
+    print("Recovered plain text :", DecryptedText(ciphertext,key))
