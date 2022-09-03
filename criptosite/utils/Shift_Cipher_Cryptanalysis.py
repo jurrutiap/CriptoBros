9f7d3dc879921873d3798df283520cf477ac4e55
Raul Peña <rpenal@unal.edu.co>
2022-09-03 17:24:37 -0500

    Shift Cryptoanalisys v3

--- criptosite/utils/Shift_Cipher_Cryptanalysis.py
+++ criptosite/utils/Shift_Cipher_Cryptanalysis.py
@@ -1,18 +1,22 @@
 import globals as g
 
 def ShiftCipherCryptanalysis(text):
+    PossibleTexts = []
     clearText = ''.join(ch for ch in text if ch.isalpha())
     textAsNumber = g.chartonum(clearText)
     for des in range(26):
         displacedText = [(char - des)%26 for char in textAsNumber]
         posibleText = g.numtochar(displacedText)
-        print(f"Assuming the key is {des}, the text is \n {posibleText}")
+        PossibleTexts.append(f"Assuming the key is {des}, the text is \n {posibleText}")
+    return PossibleTexts
 
 
 
 if __name__ == "__main__":
     message = str(input("Message:"))  # "MYSECRETMESSAGE"
-    ShiftCipherCryptanalysis(message)
+    decriptedTexts =ShiftCipherCryptanalysis(message)
+    for posib in decriptedTexts:
+        print(posib)
 
 
 
