1e0df29d79eba2f84ccee3e00487b26b24e94511
jurrutiap <jurrutia@unal.edu.co>
2022-09-03 20:06:11 -0500

    test

--- criptosite/views/views_home.py
+++ criptosite/views/views_home.py
@@ -1,3 +1,5 @@
+from ..utils.shift_system import encrypt
+
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.shortcuts import render
@@ -7,4 +9,9 @@ def home(request):
     return render(request, 'index.html')
 
 def sistema1(request):
-    return render(request, 'tables.html')
+    return render(request, 'charts.html')
+
+def shift(request, message, K):
+    if request.method == "POST":
+        data = encrypt(message, K)
+        return render(request, 'charts.html', {'data':data})
