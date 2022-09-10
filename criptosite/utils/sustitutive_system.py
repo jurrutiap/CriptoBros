6c8d737f72de4414f42cbf982f8ece36f4666f23
Odob13 <oordonezb@unal.edu.co>
2022-09-10 17:27:49 -0500

    sustitutive

--- criptosite/utils/sustitutive_system.py
+++ criptosite/utils/sustitutive_system.py
@@ -4,17 +4,19 @@ import numpy as np
 # Función principal
 
 def k(k, text):
+    text= ''.join(ch for ch in text if ch.isalpha())
     try:
-        if(int(k)>2 and int(k)<(len(text)-1)):
+        if(int(k)>2 and int(k)<(len(text))):
             K = int(k)
         else:
             raise Exception("K invalida")
     except:
-        K= np.random.randint(2,(len(text) - 1))
+        K= np.random.randint(2,(len(text)))
     return K
 
 def encrypt(key, message):
 
+    message= ''.join(ch for ch in message if ch.isalpha()).upper()
     cipherText = [''] * key
     for col in range(key):
         pointer = col
@@ -25,6 +27,7 @@ def encrypt(key, message):
 
 def desencrypt(key, message):
 
+    message= ''.join(ch for ch in message if ch.isalpha()).upper()
     numCols = m.ceil(len(message) / key)
     numRows = key
     numShadedBoxes = (numCols * numRows) - len(message)
