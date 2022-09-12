82fc6738944ccfb507f7ca6a09e977eb92efc394
Raul Peña <rpenal@unal.edu.co>
2022-09-12 17:38:19 -0500

    Affine cryptoanalisys v2

--- criptosite/utils/affine_Cryptoanalisys.py
+++ criptosite/utils/affine_Cryptoanalisys.py
@@ -1,4 +1,4 @@
-INVERTIBLENUMBERS = [1,3,5,7,9,11,15,17,19,21,23,25]
+INVERTIBLENUMBERS = {1:1,3:9,5:21,7:15,9:3,11:19,15:7,17:23,19:11,21:5,23:17,25:25}
 import globals as g
 
 def AffineCryptoanalisys(text):
@@ -9,7 +9,7 @@ def AffineCryptoanalisys(text):
         for b in range(26):
             displacedText = [(a * (char - b))%26 for char in textAsNumber]
             posibleText = g.numtochar(displacedText)
-            PossibleTexts.append(f"Assuming the keys are {a} and {b}, the text is {posibleText}")
+            PossibleTexts.append(f"Assuming the keys are {INVERTIBLENUMBERS[a]} and {b}, the text is {posibleText}")
     return PossibleTexts
 
 if __name__ == "__main__":
